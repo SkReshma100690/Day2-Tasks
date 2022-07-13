25b9ab1eb4e80e20366cbfe5716861836d3baf8c
SkReshma100690 <109241188+SkReshma100690@users.noreply.github.com>
2022-07-14 00:18:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let a=1;
+let b=2;
+console.log(a+b)
+console.log("Hello this is reshma")
+console.log("Hello this is reshma")
+console.log("Hello this is reshma")
+console.log("Hello this is reshma")
+console.log("Hello this is reshma")
---
+++ reshma.js
@@ -0,0 +1,4 @@
+console.log("This is my new program")
+let a=10;
+let b=100;
+console.log(a*b)
