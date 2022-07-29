815bce3ca8263d366809deb94e6b0a06432194d4
SkReshma100690 <109241188+SkReshma100690@users.noreply.github.com>
2022-07-29 22:06:20 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let a=1;
-let b=2;
-console.log(a+b)
-console.log("Hello this is reshma")
-console.log("Hello this is reshma")
-console.log("Hello this is reshma")
-console.log("Hello this is reshma")
-console.log("Hello this is reshma")
